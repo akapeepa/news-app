9173d5e022f575d3d8f9f2903c49c1a1c8012f7d
gurdeep singh <gurdeepsingh@gurdeeps-MacBook-Pro.local>
2023-08-11 17:51:04 -0400

    changed class based to function based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className='my-3'>
-                <div className="card">
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        position: 'absolute',
-                        right: '0'
-                    }}>
-                        <span className="badge rounded-pill bg-danger" style={{ left: '90%', zIndex: 1 }}>
-                            {source}
-                        </span>
-                    </div>
-                    <img src={!imageUrl?'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg':imageUrl} className="card-img-top" alt="news" />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className='text-muted'>By {!author ? "unknow" : author} on {new Date(date).toGMTString()}</small></p>
-                        <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-dark btn-sm">Read More</a>
-                    </div>
+const NewsItem = (props) => {
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className='my-3'>
+            <div className="card">
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0'
+                }}>
+                    <span className="badge rounded-pill bg-danger" style={{ left: '90%', zIndex: 1 }}>
+                        {source}
+                    </span>
                 </div>
-            </div >
-        )
-    }
+                <img src={!imageUrl ? 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg' : imageUrl} className="card-img-top" alt="news" />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className='text-muted'>By {!author ? "unknow" : author} on {new Date(date).toGMTString()}</small></p>
+                    <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-dark btn-sm">Read More</a>
+                </div>
+            </div>
+        </div >
+    )
 }
 
 export default NewsItem
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 import loading from './../spinner.gif'
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <div className='text-center'>
+export default function Spinner() {
+  return (
+    <div className='text-center'>
         <img className='my-3' src={loading} alt="loading" />
       </div>
-    )
-  }
+  )
 }
-
-export default Spinner
